87c03ed9b7c5439c9fc4e25cbebb886c54e7c8df
gustavohpo2016 <Sabado@1p23_18.classroom.net>
2016-10-22 11:12:23 -0200

    table view adicionada

--- testeBradesco/testeBradesco/ViewController.swift
+++ testeBradesco/testeBradesco/ViewController.swift
@@ -11,6 +11,12 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var tableview: UITableView!
+    
+  
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
